ba9dd92eb709079387d20d7e444fe4fd77768560
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-01 15:19:59 +0100

    Added utils.jl

--- src/core.jl
+++ src/core.jl
@@ -60,15 +60,3 @@ function get_L1_snapshots(order_book::PyObject)
     best_asks = np_array_to_matrix(L1["best_asks"]);
     return best_bids, best_asks;
 end
-
-"""
-    parse_logs_df(end_state::Dict)
-
-Takes the `end_state` dictionary returned by an ABIDES simulation, goes through all the agents, extracts their log,
-and un-nest them returns a single dataframe with the logs from all the agents warning: this is meant to be used 
-for debugging and exploration.
-"""
-function parse_logs_df(end_state::Dict)
-    parse_logs_df = pyimport("abides_core.utils").parse_logs_df;
-    return parse_logs_df(end_state);
-end
