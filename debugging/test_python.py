880e25cb088bf54a242a11c75c79091b34645c86
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-02 15:36:18 +0100

    added comments

--- debugging/test_python.py
+++ debugging/test_python.py
@@ -6,10 +6,14 @@ from abides_core import abides
 from abides_core.utils import parse_logs_df, ns_date, str_to_ns, fmt_ts
 from abides_markets.configs import rmsc04
 
-# Configuration and simulation
+# Build runnable configuration
 config = rmsc04.build_config(seed=0)
 config.keys()
+
+# Run simulation
 end_state = abides.run( config )
+
+# Retrieving results from `end_state`
 order_book = end_state["agents"][0].order_books["ABM"]
 
 # Get L1 and L2 snapshots
