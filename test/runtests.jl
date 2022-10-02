39f1ca0e55c6354bb4cb832fa0b04a229d476b04
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-02 18:18:14 +0100

    L2 tests implemented

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,9 +21,24 @@ using CSV, DataFrames, Dates, Statistics, Test;
     @test sum(L1.best_bids .=== L1_best_bids_benchmark) == prod(size(L1.best_bids));
     @test sum(L1.best_asks .=== L1_best_asks_benchmark) == prod(size(L1.best_asks));
 
-    # Get L2 snapshots
+    # Get L2 snapshots and compare them with the relevant Python benchmarks
     L2 = AbidesMarkets.get_L2_snapshots(order_book, 10);
+
+    # - L2 times
+    L2_times_benchmark = CSV.read("./benchmarks/rmsc04/L2_times.csv", DataFrame)[:, 2:end] |> Array{Union{Missing, Float64}};
+    @test sum(L2.times .== L2_times_benchmark) == length(L2.times);
+
+    # - L2 bids
+    L2_bids_p_benchmark = CSV.read("./benchmarks/rmsc04/L2_bids_p.csv", DataFrame)[:, 2:end] |> Array{Union{Missing, Float64}};
+    L2_bids_q_benchmark = CSV.read("./benchmarks/rmsc04/L2_bids_q.csv", DataFrame)[:, 2:end] |> Array{Union{Missing, Float64}};
+    @test L2.bids[:,:,1] == L2_bids_p_benchmark;
+    @test L2.bids[:,:,2] == L2_bids_q_benchmark;
+
+    # - L2 asks
+    L2_asks_p_benchmark = CSV.read("./benchmarks/rmsc04/L2_asks_p.csv", DataFrame)[:, 2:end] |> Array{Union{Missing, Float64}};
+    L2_asks_q_benchmark = CSV.read("./benchmarks/rmsc04/L2_asks_q.csv", DataFrame)[:, 2:end] |> Array{Union{Missing, Float64}};
+    @test L2.asks[:,:,1] == L2_asks_p_benchmark;
+    @test L2.asks[:,:,2] == L2_asks_q_benchmark;
+
     @test L2.times == some benchmark
-    @test L2.bids == some other benchmark
-    @test L2.asks == some other other benchmark
 end
