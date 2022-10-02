592a15088e3ed7939192e8f74eb4ab51297a1e1e
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-02 16:12:00 +0100

    runtests.jl template ready

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,16 +3,27 @@ include("../src/AbidesMarkets.jl");
 using Main.AbidesMarkets;
 using DataFrames, DataFramesMeta, Dates, Plots, Statistics;
 
-# Build runnable configuration
-config = AbidesMarkets.build_config("rmsc04", (seed=0, ));
-println(sort(collect(keys(config)))); # <- show keys in `config` in alphabetical order
+@testset "rmsc04 core functions" begin
 
-# Run simulation
-end_state = AbidesMarkets.run(config);
+    # Build runnable configuration
+    config = AbidesMarkets.build_config("rmsc04", (seed=0, ));
+    config_keys = sort(collect(keys(config))); # <- show keys in `config` in alphabetical order
+    @test config_keys == some benchmark
 
-# Retrieving results from `end_state`
-order_book = end_state["agents"][1].order_books["ABM"]; # Julia starts indexing from 1, not 0
+    # Run simulation
+    end_state = AbidesMarkets.run(config);
 
-# Get L1 and L2 snapshots
-L1 = AbidesMarkets.get_L1_snapshots(order_book);
-L2 = AbidesMarkets.get_L2_snapshots(order_book, 10);
+    # Retrieving results from `end_state`
+    order_book = end_state["agents"][1].order_books["ABM"]; # Julia starts indexing from 1, not 0
+
+    # Get L1 snapshots
+    L1 = AbidesMarkets.get_L1_snapshots(order_book);
+    @test L1.best_bids == some benchmark
+    @test L1.best_asks == some other benchmark
+
+    # Get L2 snapshots
+    L2 = AbidesMarkets.get_L2_snapshots(order_book, 10);
+    @test L2.times == some benchmark
+    @test L2.bids == some other benchmark
+    @test L2.asks == some other other benchmark
+end
