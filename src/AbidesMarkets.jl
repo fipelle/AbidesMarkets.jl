cf428632e8ba02c12719c079deb6386c3c7a4401
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-02 16:03:11 +0100

    More readable functions

--- src/AbidesMarkets.jl
+++ src/AbidesMarkets.jl
@@ -4,7 +4,7 @@ module AbidesMarkets
 
     # Dependencies
     using DataFrames, DataFramesMeta, Dates, Logging, MessyTimeSeries, PyCall, Statistics;
-
+        
     # Custom dependencies
     local_path = dirname(@__FILE__);
     include("$(local_path)/types.jl");
