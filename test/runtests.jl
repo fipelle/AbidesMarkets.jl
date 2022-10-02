e6e719f1beac1651cd8a35c1d5cba431bc16cd46
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-02 18:11:34 +0100

    Implemented tests for L1

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,14 +1,12 @@
 # Dependencies
 include("../src/AbidesMarkets.jl");
 using Main.AbidesMarkets;
-using Dates, Statistics, Test;
+using CSV, DataFrames, Dates, Statistics, Test;
 
 @testset "rmsc04 core functions" begin
 
     # Build runnable configuration
     config = AbidesMarkets.build_config("rmsc04", (seed=0, ));
-    config_keys = sort(collect(keys(config))); # <- show keys in `config` in alphabetical order
-    @test config_keys == some benchmark
 
     # Run simulation
     end_state = AbidesMarkets.run(config);
@@ -16,10 +14,12 @@ using Dates, Statistics, Test;
     # Retrieving results from `end_state`
     order_book = end_state["agents"][1].order_books["ABM"]; # Julia starts indexing from 1, not 0
 
-    # Get L1 snapshots
+    # Get L1 snapshots and compare them with the relevant Python benchmarks
     L1 = AbidesMarkets.get_L1_snapshots(order_book);
-    @test L1.best_bids == some benchmark
-    @test L1.best_asks == some other benchmark
+    L1_best_bids_benchmark = CSV.read("./benchmarks/rmsc04/L1_best_bids.csv", DataFrame)[:, 2:end] |> Array{Union{Missing, Float64}};
+    L1_best_asks_benchmark = CSV.read("./benchmarks/rmsc04/L1_best_asks.csv", DataFrame)[:, 2:end] |> Array{Union{Missing, Float64}};
+    @test sum(L1.best_bids .=== L1_best_bids_benchmark) == prod(size(L1.best_bids));
+    @test sum(L1.best_asks .=== L1_best_asks_benchmark) == prod(size(L1.best_asks));
 
     # Get L2 snapshots
     L2 = AbidesMarkets.get_L2_snapshots(order_book, 10);
