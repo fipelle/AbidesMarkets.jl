ddb5dc4cd56a5ae2f642ba84130652b1074bb478
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-04 14:01:40 +0100

    First draft aggregate LOB

--- src/utils.jl
+++ src/utils.jl
@@ -7,3 +7,33 @@ function parse_logs_df(end_state::Dict)
     parse_logs_df = pyimport("abides_core.utils").parse_logs_df;
     return parse_logs_df(end_state);
 end
+
+"""
+    aggregate_LOB_measurement(X::Vector{Float64}, times::DateTime, frequency::String, f::Function; f_args::Tuple, f_kwargs::NamedTuple)
+
+
+"""
+function aggregate_LOB_measurement(X::Vector{Float64}, times::DateTime, frequency::String, f::Function; f_args::Tuple, f_kwargs::NamedTuple)
+    
+    # Error management
+    if frequency ∉ ["seconds", "minutes", "hours"]
+        error("Frequency must be either 'seconds', 'minutes' or 'hours'!");
+    end
+
+    # Time aggregation with `f`
+    
+end
+
+"""
+    eop_LOB_measurement(X::Vector{Float64}, times::DateTime, frequency::String)
+
+Aggregate `X` by returing the EOP measurement taken at the specified `frequency`.
+"""
+eop_LOB_measurement(X::Vector{Float64}, times::DateTime, frequency::String) = aggregate_LOB_measurement(X, times, frequency, last);
+
+"""
+    avg_LOB_measurement(X::Vector{Float64}, times::DateTime, frequency::String)
+
+Aggregate `X` by returing the average (non-overlapping) measurement taken at the specified `frequency`.
+"""
+avg_LOB_measurement(X::Vector{Float64}, times::DateTime, frequency::String) = aggregate_LOB_measurement(X, times, frequency, mean; f_kwargs=(dims=1, ));
