f4a72e128f6c225b3f5786b6cbcdc19b653564ee
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-05 16:39:40 +0100

    New exports

--- src/AbidesMarkets.jl
+++ src/AbidesMarkets.jl
@@ -12,5 +12,7 @@ module AbidesMarkets
     include("$(local_path)/utils.jl");
     
     # Export
-    export build_config, run, get_L1_snapshots, parse_logs_df, aggregate_LOB_measurement, eop_LOB_measurement, avg_LOB_measurement;
+    export build_config, run, get_L1_snapshots, get_L2_snapshots,
+           parse_logs_df, aggregate_LOB_measurement, aggregate_LOB_measurement_eop, aggregate_LOB_measurement_avg,
+           compare_aggregated_LOB_measurements, compare_aggregated_LOB_measurements_mse;
 end
