60c8d68f0fa383507d5a8d0d742839cd768599e1
fipelle <6272230+fipelle@users.noreply.github.com>
2022-10-24 18:06:30 +0100

    Updated L2 tests to account for post-cleaning missings

--- test/runtests.jl
+++ test/runtests.jl
@@ -32,12 +32,20 @@ using CSV, DataFrames, Dates, MessyTimeSeries, Statistics, Test;
     # - L2 bids
     L2_bids_p_benchmark = CSV.read("./benchmarks/rmsc04/L2_bids_p.csv", DataFrame)[:, 2:end] |> JArray{Float64};
     L2_bids_q_benchmark = CSV.read("./benchmarks/rmsc04/L2_bids_q.csv", DataFrame)[:, 2:end] |> JArray{Float64};
-    @test L2.bids[:,:,1] == L2_bids_p_benchmark;
-    @test L2.bids[:,:,2] == L2_bids_q_benchmark;
-
+    ind_not_missing_L2_bids_p = .~ismissing.(L2.bids[:, :, 1]);
+    ind_not_missing_L2_bids_q = .~ismissing.(L2.bids[:, :, 2]);
+    @test ind_not_missing_L2_bids_p == ind_not_missing_L2_bids_q;
+    @test L2.bids[ind_not_missing_L2_bids_p, 1] == L2_bids_p_benchmark[ind_not_missing_L2_bids_p];
+    @test L2.bids[ind_not_missing_L2_bids_q, 2] == L2_bids_q_benchmark[ind_not_missing_L2_bids_q];
+    @test sum((L2_bids_p_benchmark[.~ind_not_missing_L2_bids_p] .== 0) .| (L2_bids_q_benchmark[.~ind_not_missing_L2_bids_q] .== 0)) == sum(.~ind_not_missing_L2_bids_p);
+    
     # - L2 asks
     L2_asks_p_benchmark = CSV.read("./benchmarks/rmsc04/L2_asks_p.csv", DataFrame)[:, 2:end] |> JArray{Float64};
     L2_asks_q_benchmark = CSV.read("./benchmarks/rmsc04/L2_asks_q.csv", DataFrame)[:, 2:end] |> JArray{Float64};
-    @test L2.asks[:,:,1] == L2_asks_p_benchmark;
-    @test L2.asks[:,:,2] == L2_asks_q_benchmark;
+    ind_not_missing_L2_asks_p = .~ismissing.(L2.asks[:, :, 1]);
+    ind_not_missing_L2_asks_q = .~ismissing.(L2.asks[:, :, 2]);
+    @test ind_not_missing_L2_asks_p == ind_not_missing_L2_asks_q;
+    @test L2.asks[ind_not_missing_L2_asks_p, 1] == L2_asks_p_benchmark[ind_not_missing_L2_asks_p];
+    @test L2.asks[ind_not_missing_L2_asks_q, 2] == L2_asks_q_benchmark[ind_not_missing_L2_asks_q];
+    @test sum((L2_asks_p_benchmark[.~ind_not_missing_L2_asks_p] .== 0) .| (L2_asks_q_benchmark[.~ind_not_missing_L2_asks_q] .== 0)) == sum(.~ind_not_missing_L2_asks_p);
 end
